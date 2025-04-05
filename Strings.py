63d335ddc4b118fb7a75f0e62120f87c261c87b9
bryanxxmr <bryan3094@hotmail.com>
2025-04-04 23:47:21 -0500

    2da Leccion aprendiendo de Strings con "print"

--- Strings.py
+++ Strings.py
@@ -4,3 +4,12 @@ print("Esta es una linea \ny esta es otra linea") # \n es un salto de linea
 print("\tEsta es la quinta linea") #Aqui esta tabulando
 print('This isn\'t a string') #aqui estoy indicando que la comilla la tome como un string con \'
 print('Esta signo \\ es una barra invertida') #aqui estoy escapando la barra invertida
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+
+print("Barra Normal: /")
+print("Barra Invertida: \\")
