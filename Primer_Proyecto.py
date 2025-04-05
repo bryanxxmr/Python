b8cebd28e794a6df7b013e9977badcf6c302e98c
bryanxxmr <bryan3094@hotmail.com>
2025-04-04 23:42:02 -0500

    2da Leccion aprendiendo de Strings con "print"

--- Primer_Proyecto.py
+++ Primer_Proyecto.py
@@ -4,3 +4,5 @@ print ("Me encanta estudiar Python")
 print('Estudiar con "Python Total" es super divertido')
 
 print(55+500)
+
+
---
+++ Strings.py
@@ -0,0 +1,6 @@
+print("Hola" + " " + 'Kenny')
+print("Me llamo \"Kenny\"") #aqui estoy escapando las comillas dobles
+print("Esta es una linea \ny esta es otra linea") # \n es un salto de linea
+print("\tEsta es la quinta linea") #Aqui esta tabulando
+print('This isn\'t a string') #aqui estoy indicando que la comilla la tome como un string con \'
+print('Esta signo \\ es una barra invertida') #aqui estoy escapando la barra invertida
