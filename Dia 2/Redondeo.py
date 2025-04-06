6cafb54adbddc26d6e587dc488cf863840793f0e
bryanxxmr <bryan3094@hotmail.com>
2025-04-06 13:55:18 -0500

    Dia 2 - Redondeo

---
+++ Dia 2/Redondeo.py
@@ -0,0 +1,27 @@
+resultado = 90/7
+redondeo = round(resultado)
+print(redondeo)
+
+#Ejemplo
+numero = 92.666666666
+n_redondeado = round(numero, 2)
+print(n_redondeado)
+
+
+#Ejemplo
+otro_numero = 96.666666666
+print(round(otro_numero))
+print(type(otro_numero))
+
+#Practica
+#Redondea el resultado de la división 10/3 a un número con 2 decimales, y muestra en pantalla el valor redondeado.
+print(round(10/3,2))
+
+#Practica
+#Redondea el número 10.676767 al entero más próximo, y muestra en pantalla el resultado.
+valor = 10.676767
+print(round(valor))
+
+#Practica
+#Calcula la raíz cuadrada de 5, y muestra en pantalla el resultado redondeado con 4 posiciones decimales.
+print(round(5**0.5,4))
