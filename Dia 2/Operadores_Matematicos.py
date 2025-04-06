e7416cbbb6a0e9167c309df5cfdcd2eea2ab6b5a
bryanxxmr <bryan3094@hotmail.com>
2025-04-06 13:19:29 -0500

    Dia 2 - Operadores Matematicos

---
+++ Dia 2/Operadores_Matematicos.py
@@ -0,0 +1,36 @@
+
+x = 6
+y = 2
+z = 7
+#operadores matematicos
+print(f"{x} + {y} es igual a {x + y}")
+print(f"{x} - {y} es igual a {x - y}")
+print(f"{x} * {y} es igual a {x * y}")
+print(f"{x} / {y} es igual a {x / y}")
+
+#division al piso
+print(f"{z} dividido al piso de {y} es igual a {z // y}")
+
+#modulo
+print(f"{z} modulo {y} es igual a {z % y}")
+
+#raiz cuadrada
+print(f"La raiz cuadrada de {x} es igual a {x ** (0.5)}")
+
+#potencia
+print(f"{x} elevado a {y} es igual a {x ** y}")
+
+#Ejercicio 1
+#Muestra en pantalla el cociente (división al piso) de los siguientes dos números: 874 dividido entre 27.
+#Debes mostrar solo el valor numérico que resulta de esta operación.
+print(874//27)
+
+#Ejercicio 2
+#Muestra en pantalla el módulo (es decir, el resto) de la división entre 456 y 33.
+#Debes mostrar solo el valor numérico que resulta de esta operación.
+print(456%33)
+
+#Ejercicio 3
+#Calcula y muestra en pantalla la raíz cuadrada de 783.
+#Debes mostrar solo el valor numérico que resulta de esta operación.
+print(783**0.5)
