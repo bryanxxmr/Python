ffd47f4bd169d8f19755f76b7024fa59de1b6836
bryanxxmr <bryan3094@hotmail.com>
2025-04-13 23:04:48 -0500

    Dia 4 - Operadores Logicos

---
+++ Dia 4/1Operadores_Logicos.py
@@ -0,0 +1,41 @@
+#Se va a obtener como resultado True o False
+
+#Ejemplo1 Operadores lógicos (Simbolo = simple) se le asigna un valor a una variable
+mi_variable = 'Hola Mundo'
+
+#Ejemplo2 Operadores lógicos (Simbolo == doble) se hace es una comparación
+mi_bool = 10 == 25
+#print(mi_bool)  #imprime False ya que 10 no es igual a 25
+
+#Ejemplo3 Operadores lógicos (Simbolo !=) se hace es una comparación
+variable = 10 != 25
+print(variable) #imprime True ya que 10 no es igual a 25
+
+#Practica 1
+#Crea dos variables (num1 y  num2) con los valores 36 y 17 respectivamente.
+# Verifica si num1 es mayor o igual que num2 y almacena el resultado de dicha
+# comparación en una variable llamada mi_bool
+num1 = 36
+num2 = 17
+mi_bool = num1 >= num2
+
+#Practica 2
+#Crea dos variables (num1 y  num2):
+#Dentro de num1, almacena el resultado de la operación raíz cuadrada de 25
+#Dentro de num2, almacena el número 5.
+#Verifica si num1 es igual a num2 y almacena el resultado de dicha
+#comparación en una variable llamada mi_bool.
+num1 = 25 ** 0.5
+num2 = 5
+mi_bool = num1 == num2
+
+#Practica 3
+#Crea dos variables (num1 y  num2):
+#Dentro de num1, almacena el resultado de la operación 64 x 3
+#Dentro de num2, almacena el resultado de la operación 24 x 8
+#Verifica si num1 es diferente a num2 y almacena el resultado de
+# dicha comparación en una variable llamada mi_bool.
+num1 = 64 * 3
+num2 = 24 * 8
+mi_bool = num1 != num2
+
