fabe3e134d5e3701932ac5abfc9cea6a14df8b3c
Kenny Mendo <bryan3094@hotmail.com>
2025-05-11 23:31:56 -0500

    Dia 4 - Loop o for

---
+++ Dia 4/LoopFor_repaso.py
@@ -0,0 +1,6 @@
+lista = ['Kenny', 'Bryan', 'Mendo', 'Ruiz']
+
+#Recorre la lista con un bucle for la sintaxis es: por cada elemento en la lista
+for recorrido in lista:
+    #Imprime el elemento recorrido
+    print('Hola' + recorrido)
