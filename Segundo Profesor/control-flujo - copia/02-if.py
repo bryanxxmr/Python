3d980a509fdf820e0ada2ecca042d8394b245312
Kenny Mendo <bryan3094@hotmail.com>
2025-08-08 23:43:24 -0500

    SegundaClase

---
+++ Segundo Profesor/control-flujo - copia/02-if.py
@@ -0,0 +1,9 @@
+edad = 15
+if edad > 54:
+    print("puede ver la película con descuento")
+elif edad > 17:
+    print("puede ver la película")
+else:
+    print("no puede ver la película")
+    print("Ve a otro sitio")
+print("fin del programa")
---
+++ Segundo Profesor/control-flujo - copia/05-cadena-comparadores.py
@@ -0,0 +1,5 @@
+# ver si personas mayores de 15 años de edad y menores o igual a 65 años pueden entrad a la piscina
+edad = 15
+
+if 15 <= edad <= 65:
+    print("Puede entrar a la piscina")
